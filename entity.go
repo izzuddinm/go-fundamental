0bd2bf4723e8e3b21400259c6590d5412234ae1c
Muhammad Ayom Izzuddin <ayom.izzudin@gtech.digital>
2024-11-24 14:17:58 +0700

    second commit exercise golang

---
+++ entity.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func getSentence() string {
+	return fmt.Sprintf("Hello, My name is %s and I am from %s ", "Muhammad Ayom Izzuddin", "Tuban")
+}
--- main.go
+++ main.go
@@ -1,7 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Print("Hello World")
+
+	// Declaring (Creating) Variables
+	fmt.Println("Hello, I was learn golang")
+	sentence := getSentence()
+	fmt.Println(sentence)
+
+	var numStaticType int
+	numStaticType = 10
+	fmt.Println(numStaticType)
+
+	numDynamicType := 20
+	fmt.Println(numDynamicType)
+
+	itemsCount := 5
+	price := 100.50
+	formatedString := fmt.Sprintf("itemsCount %d - price %.2f", itemsCount, price)
+	fmt.Println(formatedString)
+
+	isValid := true
+	fmt.Println(isValid)
+	var isNotValid bool = false
+	println(isNotValid)
+
+	// Go Multiple Variable Declaration
+	var a, b, c, d int = 1, 2, 3, 4
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+	var e, f = 5, "Hello World"
+	fmt.Println(e, f)
+
+	g, h := 5, "Hello World"
+	fmt.Println(g, h)
+
+	// Go Variable Declaration in a Block
+	var (
+		i int
+		j int     = 1
+		k string  = "Hello World"
+		l float64 = 120.34
+	)
+
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+	fmt.Println(l)
+
+	// Go Variable Naming Rules
+	// Camel Case
+	var myVariableName string = "Muhammad Ayom Izzuddin"
+	fmt.Println(myVariableName)
+
+	// Pascal Case
+	var MyVariableName string = "Muhammad Ayom Izzuddin"
+	fmt.Println(MyVariableName)
+
+	// Snake Case
+	var my_variable_name string = "Muhammad Ayom Izzuddin"
+	fmt.Println(my_variable_name)
+
+	// Go Contants
+	const MIN_RETRY int = 3
+	const MAX_RETRY int = 9
+	fmt.Println(MIN_RETRY + MAX_RETRY)
+
+	var nums = 15.5
+	var txt = "Hello World!"
+
+	// General Formatting Verbs
+	fmt.Printf("%v\n", nums)
+	fmt.Printf("%#v\n", nums)
+	fmt.Printf("%v%%\n", nums)
+	fmt.Printf("%T\n", nums)
+
+	fmt.Printf("%v\n", txt)
+	fmt.Printf("%#v\n", txt)
+	fmt.Printf("%T\n", txt)
+
 }
