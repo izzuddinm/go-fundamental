7349ed5718db2abedfcd4b5f83513162fb8be97f
Muhammad Ayom Izzuddin <ayom.izzudin@gtech.digital>
2024-11-28 14:27:04 +0700

    add changes

--- import/main.go
+++ import/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go-fundamental/helper"
 	"go-fundamental/modifier"
+	"time"
 )
 
 func main() {
@@ -19,4 +20,22 @@ func main() {
 	// tidak bisa diakses karena nama depan variable nya menggunakan huruf kecil
 	// version := modifier.version
 	// modifier2 := modifier.substractTotalMessage(1, 2)
+
+	var uniqueCodes = []string{}
+	fmt.Println(uniqueCodes)
+
+	// Use a map to track unique codes
+	uniqueCodeMap := make(map[string]struct{})
+
+	for len(uniqueCodes) < 10 {
+		uniqueCode := fmt.Sprintf("%d", time.Now().UnixNano()/1000)
+		if _, exists := uniqueCodeMap[uniqueCode]; !exists {
+			uniqueCodes = append(uniqueCodes, uniqueCode)
+			uniqueCodeMap[uniqueCode] = struct{}{} // Mark this code as used
+		}
+		// Sleep for a short duration to ensure the next code is different
+		time.Sleep(1 * time.Millisecond)
+	}
+
+	fmt.Println(uniqueCodes)
 }
